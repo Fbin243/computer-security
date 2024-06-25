3283f1073d8fa4b6239f285a429f748a3c28ecac
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-06-25 10:27:30 +0700

    feat: decrypt Kx

--- src/ui/DecryptForm.java
+++ src/ui/DecryptForm.java
@@ -1,11 +1,18 @@
 package ui;
 
 import interfaces.IFileInputEvent;
+import logic.AES;
+import logic.RSA;
 
 import java.awt.Dimension;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import javax.swing.Box;
+
+import constants.Common;
 
 public class DecryptForm extends Form {
 	private final FileInput fileInputPrivateKey;
@@ -22,10 +29,7 @@ public class DecryptForm extends Form {
 		});
 		fileInputPrivateKey.setFileInputLabel("Upload File Private Key");
 		actionButton.setText("Decrypt");
-		actionButton.addActionListener(e -> {
-			System.out.println("Decrypting file...");
-			System.out.println("File: " + selectedFileForm.getAbsolutePath());
-		});
+		actionButton.addActionListener(e -> handleDecrypt());
 
 		mainPanel.add(header);
 		mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
@@ -35,4 +39,23 @@ public class DecryptForm extends Form {
 		mainPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 		mainPanel.add(actionButton);
 	}
+
+	private void handleDecrypt() {
+        try {
+
+			// Compare SHA-1 and HKprivate - Binh
+           
+            // If SHA-1 == HKprivate then
+			// Decrypt the AES key
+            // RSA rsa = new RSA();
+            // rsa.loadPrivateKeyFromString(privateKey);
+            // String aesKey = rsa.decrypt(encryptedAesKey);
+
+            // Decrypt the file using AES - Tuan
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Decryption failed.");
+        }
+    }
 }
